4346874cf16727dbde7555fa5e0f48b610e18772
Sumeet36 <55999226+Sumeet36@users.noreply.github.com>
2020-08-23 01:47:09 +0530

    Add files via upload

---
+++ Assistant.py
@@ -0,0 +1,36 @@
+import pyttsx3
+import os
+
+pyttsx3.speak("Hello User!")
+pyttsx3.speak("This is Nova, your personal system assistant, How may I help you?")
+
+
+while True:
+    p = input("chat with me with your requirements : ")
+
+    if ((("run" in p) or ("open" in p )) and ("chrome" in p)) :
+        pyttsx3.speak("Opening Chrome")
+        os.system("chrome")
+
+    elif (("run" in p) or  ("open" in p )) and (("notepad" in p) or ("editor" in p) ) :
+        pyttsx3.speak("Opening Notepad")
+        os.system("notepad")
+
+    elif ('facebook' in p):
+        pyttsx3.speak('opening facebook')
+        os.system('start  http://www.facebook.com/')
+
+    elif ('notepad++' in p) or ('pad editor' in p):
+        pyttsx3.speak('Launching your notepad++')
+        os.system('notepad++')
+
+    elif (("run" in p) or  ("open" in p ))  and ("player" in p) and ("media" in p) :
+        pyttsx3.speak("Opening Media Player")
+        os.system("wmplayer")
+
+    elif ("exit" in p)  or ("quit" in p):
+        pyttsx3.speak("Happy to help you. See you around")
+        break
+
+    else:
+        pyttsx3.speak("not supported, Please try again!")
